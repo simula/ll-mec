19a00d53ddfef8b20ddaf26f0c7cedc47fc00ce7
Anta Huang <anta.huang@eurecom.fr>
2017-10-11 18:07:34 +0200

    Implemented UE setup call

--- src/north_api/ue_rest_calls.cc
+++ src/north_api/ue_rest_calls.cc
@@ -1,5 +1,10 @@
 #include <pistache/http.h>
 #include "ue_rest_calls.h"
+#include "json.h"
+#include <iostream>
+#include <string>
+
+using json = nlohmann::json;
 
 namespace llmec {
 namespace north_api {
@@ -11,10 +16,29 @@ namespace north_api {
   }
 
   void Ue_rest_calls::add_ue(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response) {
-    std::string ue_identities_json = request.body();
+    std::string resp;
+    json ue_identities_json = json::parse(request.body());
 
-    std::string resp = "OK";
-    response.send(Pistache::Http::Code::Ok, resp);
+    if ( (!ue_identities_json["s1_ul_teid"].empty() && ue_identities_json["s1_ul_teid"].is_string())
+        && (!ue_identities_json["s1_dl_teid"].empty() && ue_identities_json["s1_dl_teid"].is_string())
+        && (!ue_identities_json["ue_ip"].empty() && ue_identities_json["ue_ip"].is_string())
+        && (!ue_identities_json["enb_ip"].empty() && ue_identities_json["enb_ip"].is_string())
+        ) {
+      std::cout<<ue_identities_json["s1_ul_teid"].dump()<<std::endl;
+      std::cout<<ue_identities_json["s1_dl_teid"].dump()<<std::endl;
+      uint64_t s1_ul_teid = std::stoul(ue_identities_json["s1_ul_teid"].dump(), nullptr, 16);
+      uint64_t s1_dl_teid = std::stoul(ue_identities_json["s1_dl_teid"].dump(), nullptr, 16);
+      std::string ue_ip = ue_identities_json["ue_ip"];
+      std::string enb_ip = ue_identities_json["enb_ip"];
+      std::cout<<s1_ul_teid<<" "<<s1_dl_teid<<" "<<ue_ip<<" "<<enb_ip<<std::endl;
+      this->ue_manager->add_ue(s1_ul_teid, s1_dl_teid, ue_ip, enb_ip);
+      std::string resp = "OK";
+      response.send(Pistache::Http::Code::Ok, resp);
+      }
+    else {
+      resp = "Format error.";
+      response.send(Pistache::Http::Code::Bad_Request, resp);
+    }
   }
   void Ue_rest_calls::get_ue(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response) {
     std::string resp = "Not implemented yet";
