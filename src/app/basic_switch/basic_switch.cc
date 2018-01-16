4ead84f9fe89e9711f58ebcb9535db45e3cf7b6e
Anta Huang <anta.huang@eurecom.fr>
2018-01-16 15:12:30 +0100

    Attempt to have multiple switch concept

--- src/app/basic_switch/basic_switch.cc
+++ src/app/basic_switch/basic_switch.cc
@@ -8,7 +8,11 @@ namespace basic_switch {
 void Basic_switch::event_callback(llmec::core::eps::ControllerEvent* ev) {
   if (ev->get_type() == llmec::core::eps::EVENT_SWITCH_UP) {
     this->of_interface.install_default_flow(ev->of_conn_);
+    switch_set_.insert(ev->of_conn_->get_id());
   }
+}
+std::unordered_set<int> Basic_switch::get_switch_list() {
+  return this->switch_set_;
 }
 void Basic_switch::start() {
   while(true){
--- src/app/basic_switch/basic_switch.h
+++ src/app/basic_switch/basic_switch.h
@@ -1,6 +1,6 @@
 #ifndef __BASIC_SWITCH_H__
 #define __BASIC_SWITCH_H__
-
+#include <unordered_set>
 #include "app.h"
 
 namespace llmec {
@@ -11,7 +11,10 @@ class Basic_switch : public llmec::app::App {
   public:
     Basic_switch(llmec::core::eps::OFInterface &of_interface) : llmec::app::App(of_interface) {}
     void event_callback(llmec::core::eps::ControllerEvent* ev);
+    std::unordered_set<int> get_switch_list();
     void start() override;
+  private:
+    std::unordered_set<int> switch_set_;
 };
 
 } // namespace basic_switch
