8bfb70cdfbb2cb3c7cc1b6111081ea5afe91db25
Anta Huang <anta.huang@eurecom.fr>
2018-02-13 12:37:43 +0100

    Add API documentation into build and install_dependencies

--- build_ll-mec.sh
+++ build_ll-mec.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+unset $DOCS
+
+while getopts ":d" option; do
+  case "${option}" in
+    d)
+      DOCS=1
+      ;;
+  esac
+done
+
+if [ -n "$DOCS" ]; then
+  echo "Generating API documentation"
+  apidoc -i src/ -o ./docs -f ".*\\.cc$" -f ".*\\.h$"
+fi
+
 mkdir -p build
 cd build
 cmake ..
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,5 +9,24 @@ function install_pistache {
     sudo make install
     cd -
 }
+
+sudo apt update
+
+unset $DOCS
+
+while getopts d option
+do
+  case "${option}"
+  in
+    d) DOCS=1;;
+  esac
+done
+
+if [ -z $DOCS ]; then
+  echo "Installing apidocs and its dependencies"
+  sudo apt install npm -y
+  sudo npm install apidoc -g
+fi
+
 sudo apt install libssl-dev libevent-dev cmake git build-essential g++ -y
 install_pistache
